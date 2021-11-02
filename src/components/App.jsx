c775205ed33578fe99086ca24bda4b1c755c30ce
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-11-02 16:12:29 -0400

    Finished up video 5, added titlebar-nav, with several book information

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,32 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+           {title: "The Hobbit", author: 'J.R.R Tolkien'},
+           {title: "1984", author: 'George Orwell'},
+           {title: "The Handmaid's Tale", author: 'Margaret Atwood'}
+        ];
+        this.state = {
+            bookNumber: 0
+        };
     }
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="row">
+                <div className="col-md-4">
+                    {/*Button previous*/}
+                </div>
+                <div className="col-md-4">
+                    {/*Display Book*/}
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                </div>
+                <div className="col-md-4">
+                    {/*Button next*/}
+                    </div>
+                </div>
             </div>
         )
     }
